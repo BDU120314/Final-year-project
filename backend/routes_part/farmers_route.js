c36e8a83af72e954427705015ffb7655b9d41922
BDU120314 <tsecha6@gmail.com>
2023-05-04 06:01:57 +0300

    formatted

--- backend/routes_part/farmers_route.js
+++ backend/routes_part/farmers_route.js
@@ -1,121 +1,12 @@
 const express = require("express");
 const db = require("../config/connection_db");
+const { CreateFarmers, DeleteFarmers, getAllFarmers, GetSingleFarmer } = require("../controllers/farmers_controller");
 const router = express.Router();
 
-//create farmer
-router.post("/", (req, res) => {
-  const {
-    fname,
-    mname,
-    lname,
-    birth_date,
-    email,
-    address,
-    phone_number,
-    land_amount,
-    user_name,
-    password,
-    id,
-  } = req.body;
-
-  var sql =
-    "INSERT INTO farmers (fname, mname, lname, birth_date, email, address, phone_number, land_amount, user_name,password, id) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
-  db.query(
-    sql,
-    [
-      fname,
-      mname,
-      lname,
-      birth_date,
-      email,
-      address,
-      phone_number,
-      land_amount,
-      user_name,
-      password,
-      id,
-    ],
-    (error, result) => {
-      if (!error) {
-        console.log("Data added successfully!");
-      } else {
-        console.log(error.message);
-      }
-    }
-  );
-});
-
-//Creating GET Router to fetch all the learner details from the MySQL Database
-router.get("/", (req, res) => {
-  db.query("SELECT * FROM farmers", (err, rows, fields) => {
-    if (!err) {
-      res.send(rows);
-    } else console.log(err);
-  });
-});
-//get single farmer
-router.get("/:id", (req, res) => {
-  const { id } = req.params.id;
-  const sql = `SELECT * FROM farmers where id = "${id}"`;
-  db.query(sql, (err, data, field) => {
-    if (!err) {
-      res.send(data);
-      console.log(`user exist in id ${id}`);
-    } else console.log(err);
-  });
-});
-
-//update farmers info
-router.put("/update/:id", (req, res) => {
-  const { id } = req.params.id;
-  const {
-    fname,
-    mname,
-    lname,
-    birth_date,
-    email,
-    address,
-    phone_number,
-    land_amount,
-    user_name,
-    password,
-  } = req.body;
-  // Update query to update multiple fields
-  const sql = `UPDATE farmers SET fname = ?, mname = ?, lname = ? birth_date = ?, email = ?, address = ?  phone_number =?,
-    land_amount = ?, 
-    user_name = ?,
-    password = ?, WHERE id = ?`;
-  db.query(
-    sql,
-    [
-      fname,
-      mname,
-      lname,
-      birth_date,
-      email,
-      address,
-      phone_number,
-      land_amount,
-      user_name,
-      password,
-    ],
-    (error, result) => {
-      if (error) {
-        console.error(error);
-        res.status(500).send("Server error: Could not update farmer data");
-      } else {
-        res.status(200).send("User data updated successfully");
-      }
-    }
-  );
-});
-
-// //delete farmer from data base
-// router.use("/delete/:id",
-// (req, res) =>{
-//   const {id} = req.params
-//   db.query(DROP farmers where id= id)
-
-//   })
+router.post("/", CreateFarmers);
+router.get("/", getAllFarmers);
+router.get("/:id", GetSingleFarmer);
+router.put("/update/:id", );
+router.delete("/delete/:id", DeleteFarmers);
 
 module.exports = router;
