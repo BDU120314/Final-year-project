52dfbac9ee57b174c5b9e3c9185f1875918c0682
BDU120314 <tsecha6@gmail.com>
2023-05-05 03:20:04 +0300

    updated some feactures

---
+++ frontend/src/components/displayData/farmers_data.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./farmers_data.css";
+const FarmersData = () => {
+  const [farmersData, setFarmersData] = useState([]);
+
+  const handleDelete = (id) => {
+    axios
+      .delete(`http://localhost:5000/api/v1/farmers/delete/${id}`)
+      .then((response) => {
+        setFarmersData(farmersData.filter((item) => item.id !== id));
+        console.log(`deleted user id :${id}`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/v1/farmers")
+      .then((response) => {
+        setFarmersData(response.data);
+        console.log(farmersData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [farmersData]);
+
+  return (
+    <div className="container">
+      <table>
+        <thead>
+          <tr>
+            <th>First NAme</th>
+            <th>Middle Name</th>
+            <th>Email Address</th>
+            <th>Phone Number</th>
+            <th>User Name</th>
+            <th>ID</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {farmersData.map((datas) => {
+            return (
+              <tr key={datas.id}>
+                <td>{datas.fname} </td>
+                <td>{datas.mname}</td>
+                <td>{datas.email}</td>
+                <td>{datas.phone_number}</td>
+                <td>{datas.user_name}</td>
+                <td>{datas.id}</td>
+                <td>
+                  <button className="btn edit">Edit</button>
+                  <button className="btn view">View</button>
+                  <button
+                    className="btn delete"
+                    onClick={() => handleDelete(datas.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default FarmersData;
